316b89a21775dd07e5472b1ca9e10e4029f3d9be
biniruu <biniruu@gmail.com>
2023-03-21 17:12:26 +0900

    chore: update meta data on gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -51,9 +51,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `인턴 B의 학습일지`,
+        short_name: `study-log`,
+        description: `팀장님도 인용하는 블로그를 쓰는 그날까지!!`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -123,7 +123,7 @@ const config: GatsbyConfig = {
   }
 }`,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `인턴 B의 학습일지`,
           },
         ],
       },
